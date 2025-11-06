7d3f2cd43518da31922f5dfc777e820ab93f1b17
Docile123 <imbereyemasoo@gmail.com>
2025-11-06 22:51:11 +0200

    feat:add the set up pizza shop

---
+++ pizza shop/index.js
@@ -0,0 +1,38 @@
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+];
+let cashInRegister = 100;
+let nextOrderId = 1;
+let orderQueue = [];
+function addNewPizza(pizzaObj) {
+    menu.push(pizzaObj);
+}
+function placeOrder(pizzaName) {
+    let selectedPizza = menu.find(pizza => pizza.name === pizzaName);
+    if (selectedPizza) {
+        cashInRegister += selectedPizza.price;
+        const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "Ordered" };
+        orderQueue.push(newOrder);
+        return newOrder;
+    }
+}
+function completeOrder(orderId) {
+    let order = orderQueue.find(pizza => pizza.id === orderId);
+    if (order) {
+        order.status = "completed";
+    }
+    return order;
+}
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ name: "BBQ Chicken", price: 12 });
+addNewPizza({ name: "Spicy Sausage", price: 11 });
+placeOrder("Chicken Bacon Ranch");
+completeOrder(1);
+console.log("Menu:", menu);
+console.log("Cash in register:", cashInRegister);
+console.log("Order queue:", orderQueue);
+export {};
+//# sourceMappingURL=index.js.map
---
+++ pizza shop/index.ts
@@ -0,0 +1,66 @@
+interface Pizza{
+    name:string;
+    price:number
+}
+
+interface Order{
+    id:number;
+    pizza:Pizza;
+    status:string
+}
+
+const menu:Pizza[] = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+let cashInRegister:number= 100;
+let nextOrderId =1;
+let orderQueue:Order[] = []
+
+function addNewPizza(pizzaObj:Pizza) {
+    menu.push(pizzaObj)
+}
+
+
+
+
+
+
+ function placeOrder(pizzaName:string){
+     let selectedPizza=menu.find(pizza=>pizza.name===pizzaName);
+     if(selectedPizza){
+          cashInRegister+=selectedPizza.price;
+           const newOrder:{id:number,pizza:Pizza,status:string} = {id:nextOrderId++,pizza:selectedPizza,status:"Ordered"};
+     
+     orderQueue.push(newOrder);
+     return newOrder;
+     }
+   
+    
+     
+ }
+ 
+ 
+ function completeOrder(orderId:number){
+     let order = orderQueue.find(pizza=>pizza.id===orderId);
+     if(order){
+       order.status="completed";
+     }
+     
+     return order;
+ }
+ 
+ 
+ addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
+
+placeOrder("Chicken Bacon Ranch")
+completeOrder(1)
+
+console.log("Menu:", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order queue:", orderQueue)
+ 
